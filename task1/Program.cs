4bf2be9057404fca31cc8dee7f3e9787d6675a49
Polina Drokina <sukhova2016@inbox.ru>
2022-11-04 16:17:08 +0400

    Добавлено решение первой задачи

---
+++ task1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите количество элементов массива: ");
+int size = Convert.ToInt16(Console.ReadLine());
+Random r = new Random();
+int[] array = new int[size];
+int sum = 0;
+{
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = r.Next(-20, 20);
+        Console.Write(array[i] + " ");
+    }
+    for (int i = 0; i < size; i++)
+    {
+        if (i % 2 != 0)
+        sum += array[i];
+    }
+    Console.WriteLine();
+    Console.Write($"Сумма нечетных элементов массива= {sum}");
+}

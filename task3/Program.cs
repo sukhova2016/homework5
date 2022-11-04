84d88688c3e8d8790356bc8744741d7bf8265da8
Polina Drokina <sukhova2016@inbox.ru>
2022-11-04 17:59:16 +0400

    Добавлено решение третьей задачи

---
+++ task3/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.Write("Введите количество элементов массива: ");
+int size = Convert.ToInt16(Console.ReadLine());
+Random r = new Random();
+double[] numbers= new double[size];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > max)
+        {
+            max = numbers[i];
+        }
+    if (numbers[i] < min)
+        {
+            min = numbers[i];
+        }
+}
+
+Console.WriteLine($"Разница между максимальным и минимальным значением = {max} - {min} = {max-min}");
+
+void FillArrayRandomNumbers(double[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+        }
+}
+void PrintArray(double[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
